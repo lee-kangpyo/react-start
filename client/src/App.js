33435b1f1b00c1c8593886b4db1a2f6b7e9e7b59
그램 <darkzhon@hanmail.net>
2022-08-03 01:04:46 +0900

    리액트 프로젝트 시작

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Start React</h1>
+      <p>html 적용하기</p>
+    </div>
+  );
+}
+
+export default App;
