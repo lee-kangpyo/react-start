9b0015bc2980f52209a139933afee158a1f8cb81
그램 <darkzhon@hanmail.net>
2022-08-03 23:30:16 +0900

    리액트 콤포넌트 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ImportComponent from './R003_ImportComponent'
 
 function App() {
   return (
     <div>
       <h1>Start React</h1>
       <p>html 적용하기</p>
+      <ImportComponent></ImportComponent>
     </div>
   );
 }
---
+++ client/src/R003_ImportComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class R003_ImportComponent extends React.Component {
+  render () {
+    return(
+        <h2>[이것은 import된 콤퍼넌트입니다.]</h2>
+    )
+  }
+}
+
+export default R003_ImportComponent;
