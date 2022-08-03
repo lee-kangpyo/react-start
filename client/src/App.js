695392942381eae826b977521867cbbf8a9a94d1
그램 <darkzhon@hanmail.net>
2022-08-04 00:54:45 +0900

    콤포넌트 폴더로 이동 및 ES6 추가된 함수들

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import ImportComponent from './R003_ImportComponent'
-import LifeCycle from './R008_Lifecycle'
+import Es6 from './component1/R009_Es6'
 
 function App() {
   return (
     <div>
       <h1>Start React</h1>
-      <p>html 적용하기</p>
-      <ImportComponent></ImportComponent>
-      <LifeCycle
-        prop_value = "FromApp.js"
-      />
+      <p>ES6 적용하기</p>
+      <Es6 />
     </div>
   );
 }
