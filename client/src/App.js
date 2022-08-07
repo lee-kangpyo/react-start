faa305167f288d3f00a686855a5bbfc86e800631
그램 <darkzhon@hanmail.net>
2022-08-07 18:42:00 +0900

    reactstrap Button Dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBreadcrumbs from './component2/R036_ReactstrapBreadcrumbs'
+import ReactstrapDropdown from './component2/R037_ReactstrapDropdown'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
       <h1>Start React</h1>
-      <ReactstrapBreadcrumbs />
+      <ReactstrapDropdown />
     </div>
   );
 }
