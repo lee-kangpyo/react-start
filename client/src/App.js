514c431d91ae488f7fc01567af3c157ebdbf6610
그램 <darkzhon@hanmail.net>
2022-08-07 17:58:22 +0900

    reactstrap Breadcrumbs

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBadge from './component2/R035_ReactstrapBadge'
+import ReactstrapBreadcrumbs from './component2/R036_ReactstrapBreadcrumbs'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
       <h1>Start React</h1>
-      <ReactstrapBadge />
+      <ReactstrapBreadcrumbs />
     </div>
   );
 }
