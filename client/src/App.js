5250a3189b82d016b9366683a1a82af1c85447e8
그램 <darkzhon@hanmail.net>
2022-08-07 17:26:12 +0900

    reactstrap Badge

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactStrapAlerts from './component2/R034_ReactstrapAlerts'
+import ReactstrapBadge from './component2/R035_ReactstrapBadge'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
       <h1>Start React</h1>
-      <ReactStrapAlerts />
+      <ReactstrapBadge />
     </div>
   );
 }
