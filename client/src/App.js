0b8da93a0ced929dd5303a5838ab2ea0dfa34b63
그램 <darkzhon@hanmail.net>
2022-08-17 00:37:22 +0900

    reactstrap Modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactstrapListGroup from './component2/R047_ReactstrapListGroup'
+import ReactstrapModal from './component2/R048_ReactstrapModal'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className='app'>
       <h1 className='title'>Start React</h1>
-      <ReactstrapListGroup />
+      <ReactstrapModal />
     </div>
   );
 }
