faa09ac5f0bb5834bb2cef6cc2e667e9c287a8a2
그램 <darkzhon@hanmail.net>
2022-08-19 02:24:17 +0900

    sweetalert2 포지셔닝

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Sweetalert2Basic from './component2/R056_Sweetalert2Basic'
+import Sweetalert2Position from './component2/R057_Sweetalert2Position'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className='app'>
       <h1 className='title'>Start React</h1>
-      <Sweetalert2Basic />
+      <Sweetalert2Position />
     </div>
   );
 }
