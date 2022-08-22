aac2e1249535161f7481e2cedc0427cd02759a81
그램 <darkzhon@hanmail.net>
2022-08-23 01:05:12 +0900

    axios get

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import FetchPost from './component2/R060_FetchPost'
+import AxiosGet from './component2/R061_AxiosGet'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className='app'>
       <h1 className='title'>Start React</h1>
-      <FetchPost />
+      <AxiosGet />
     </div>
   );
 }
