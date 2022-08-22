8492747cf2e393bec1d1a29bc84e100392d8e575
그램 <darkzhon@hanmail.net>
2022-08-22 22:56:02 +0900

    fetch get

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import SweetAlert2Confirm from './component2/R058_SweetAlert2Confirm'
+import FetchGet from './component2/R059_FetchGet'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className='app'>
       <h1 className='title'>Start React</h1>
-      <SweetAlert2Confirm />
+      <FetchGet />
     </div>
   );
 }
