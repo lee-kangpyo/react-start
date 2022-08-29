082e83bc7b6f13972ed9e2452f0e0e09aeb8a564
그램 <darkzhon@hanmail.net>
2022-08-30 01:08:46 +0900

    키이벤트 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactonMouseOut from './component3/R070_onMouseOut'
+import OnKey from './component3/R071_OnKey'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className='app'>
       <h1 className='title'>Start React</h1>
-      <ReactonMouseOut />
+      <OnKey />
     </div>
   );
 }
