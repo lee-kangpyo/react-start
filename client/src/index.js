b2da37bcb5a9a45277b5e6e1408cf96688d071b8
그램 <darkzhon@hanmail.net>
2022-09-06 01:14:06 +0900

    redux 미들웨어

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const CallMiddleWare = store => nextMiddle => action => {
+  console.log("1. reducer 실행 전");
+  console.log('2. action.type : ' + action.type+', store str : '+store.getState().data.str);
+  let result = nextMiddle(action);
+  console.log('3. reducer 실행 후');
+  console.log('4. action.type : ' + action.type+', store str : '+store.getState().data.str);
+  return result;
+}
+
+const store = createStore(reducers, applyMiddleware(CallMiddleWare));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
