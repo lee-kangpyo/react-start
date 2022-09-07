2e41014bd12f30095b1307a2c15554bf1165063e
그램 <darkzhon@hanmail.net>
2022-09-08 01:24:30 +0900

    리덕스 사용했던 app.js 백업

---
+++ client/src/reducers/App_redux예제.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import StrAddButton from "../StrAddButton";
+
+function App(props) {
+  return (
+      <div>
+          <h1>리액트 시작</h1>
+          {/* <span>{props.store.getState().data.str}</span><br/>
+          <StrAddButton store={props.store}/> */}
+          <span>{props.str} {props.test}</span><br/>
+          <StrAddButton AppProp="789"/>
+      </div>
+  );
+
+}
+
+let mapStateToProps = (state, props) => {
+  console.log('props from index.js : ' + props.indexProp)
+  return {
+    str:state.data.str,
+    test:state.data.test
+  }
+}
+
+App = connect(mapStateToProps, null)(App);
+
+export default App;
