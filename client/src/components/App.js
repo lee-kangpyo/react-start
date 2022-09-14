1ba825d49dbba3250262f9d7a0415c981ce448bf
그램 <darkzhon@hanmail.net>
2022-09-15 03:16:27 +0900

    header footer 셋팅

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import ReactRouter from './component3/R089_reactRouter'
-import ReactRouter2 from './component3/R089_reactRouter2'
+import ReactRouter from './R089_reactRouter'
+import ReactRouter2 from './R089_reactRouter2'
+
+// css
+import '../css/new.css';
+
+import HeaderAdmin from './Header/Header admin';
+
+import Footer from './Footer/Footer';
 
 function App(props) {
   return (
       <div className='App'>
+        <HeaderAdmin />
         <Routes>
           <Route exact path="/" element={<ReactRouter/>} />
           <Route exact path='/reactRouter2' element={<ReactRouter2/>} />
         </Routes>
+        <Footer />
       </div>
   );
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,39 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import reducers from './reducers';
 
-const CallMiddleWare = store => nextMiddle => action => {
-  console.log("1. reducer 실행 전");
-  console.log('2. action.type : ' + action.type+', store str : '+store.getState().data.str);
-  let result = nextMiddle(action);
-  console.log('3. reducer 실행 후');
-  console.log('4. action.type : ' + action.type+', store str : '+store.getState().data.str);
-  return result;
-}
-
-const store = createStore(reducers, applyMiddleware(CallMiddleWare));
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const listener = () => {
-  root.render(
-    <Provider store = {store}>
-      <BrowserRouter>
-        <App indexProp="react" />
-      </BrowserRouter>
-    </Provider>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './components/App';
 
 
-store.subscribe(listener);
-listener();
+ReactDOM.render((
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
