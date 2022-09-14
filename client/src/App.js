28b879fed4bcbfa2af1180d1d46ca7771bb0ea61
그램 <darkzhon@hanmail.net>
2022-09-14 22:43:55 +0900

    react-router-dom  사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import CookieRemove from './component3/R087_cookieRemove'
+import { Route, Routes } from "react-router-dom";
+import ReactRouter from './component3/R089_reactRouter'
+import ReactRouter2 from './component3/R089_reactRouter2'
 
 function App(props) {
   return (
-      <div>
-          <CookieRemove/>
+      <div className='App'>
+        <Routes>
+          <Route exact path="/" element={<ReactRouter/>} />
+          <Route exact path='/reactRouter2' element={<ReactRouter2/>} />
+        </Routes>
       </div>
   );
 
--- client/src/reducers/index_redux예제.js
+++ client/src/reducers/index_redux예제.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -24,9 +23,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const listener = () => {
   root.render(
     <Provider store = {store}>
-      <BrowserRouter>
         <App indexProp="react" />
-      </BrowserRouter>
     </Provider>
   );
 }
