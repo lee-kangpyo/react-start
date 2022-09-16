091956e214ac01cd89b8b31fc99fc2c075a9b81f
그램 <darkzhon@hanmail.net>
2022-09-17 01:54:29 +0900

    lodash throttle 사용하기

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,6 +12,7 @@ import Footer from './Footer/Footer';
 import LoginForm from './LoginForm'
 
 import ReactDebounce from "./R094_reactDebounce";
+import ReactThrottle from "./R095_reactThrottle";
 
 function App(props) {
   return (
@@ -20,6 +21,7 @@ function App(props) {
         <Routes>
           <Route exact path="/" element={<LoginForm/>} />
           <Route exact path="/Debounce" element={<ReactDebounce/>} />
+          <Route exact path="/Throttle" element={<ReactThrottle/>} />
         </Routes>
         <Footer />
       </div>
