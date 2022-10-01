502d1947e541427ff7d34f12b2b56a84faec43d3
그램 <darkzhon@hanmail.net>
2022-10-01 22:36:21 +0900

    concurrently로 react, node 서버 한번에 구동하기(nodeMon 사용)

--- server.js
+++ server.js
@@ -9,4 +9,7 @@ var app = express();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
 module.exports = app;
